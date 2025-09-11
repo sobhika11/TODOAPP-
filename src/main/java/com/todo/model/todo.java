f8a4e860c6bcadc19930f337952f0ac2b289ca93
sobhika11 <sobhika1105@gmail.com>
2025-09-11 16:20:54 +0530

    Initial commit

---
+++ src/main/java/com/todo/model/todo.java
@@ -0,0 +1,72 @@
+package com.todo.model;
+
+import java.time.LocalDateTime;
+
+public class todo {
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+    public  todo()
+    {
+         this.created_at=LocalDateTime.now();
+         this.updated_at=LocalDateTime.now();
+    }
+    public todo(String title, String description) {
+        this();
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+    public todo(int id, String title, String description, boolean completed, LocalDateTime created_at, LocalDateTime updated_at) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+
+
+    
+}
